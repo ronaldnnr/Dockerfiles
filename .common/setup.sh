a01db9158899d8902d0929b10f260c79444395b8
root <root@ip-172-31-11-6.us-east-2.compute.internal>
2021-02-28 05:53:48 +0000

    Upload image ansible

--- .common/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
-composer self-update
-apk add --no-cache bash gosu git apk-tools --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/
-mkdir -p $COMPOSER_HOME /var/app
-chown -R nobody:nobody $COMPOSER_HOME /var/app
